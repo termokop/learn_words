de0395fb6bbea68e768b68962c0f115e506b283e
termokop <termokop564@gmail.com>
2023-10-05 09:29:17 -0600

    add styles for vWordCard component

--- src/components/vWordCard.vue
+++ src/components/vWordCard.vue
@@ -41,23 +41,114 @@
 
 <template>
     <div class="card">
-        <button @click="revers_card=!revers_card">reverse</button>
-        <div class="word-card-front" v-if="!revers_card">
-            
-            <input autofocus @keydown.enter="check_ans()" type="text" name="word" id="word" v-model="word_ans">
-            <div class="word-type">{{ words_arr[index].type }}</div>
+        <button class="reverse-btn" @click="revers_card=!revers_card"><svg fill="white" width="20px" height="20px" viewBox="-1 -4.5 24 24" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin" class="jam jam-refresh-reverse"><path d='M4.859 5.308l1.594-.488a1 1 0 0 1 .585 1.913l-3.825 1.17a1 1 0 0 1-1.249-.665L.794 3.413a1 1 0 1 1 1.913-.585l.44 1.441C5.555.56 10.332-1.035 14.573.703a9.381 9.381 0 0 1 5.38 5.831 1 1 0 1 1-1.905.608A7.381 7.381 0 0 0 4.86 5.308zm12.327 8.195l-1.775.443a1 1 0 1 1-.484-1.94l3.643-.909a.997.997 0 0 1 .61-.08 1 1 0 0 1 .84.75l.968 3.88a1 1 0 0 1-1.94.484l-.33-1.322a9.381 9.381 0 0 1-16.384-1.796l-.26-.634a1 1 0 1 1 1.851-.758l.26.633a7.381 7.381 0 0 0 13.001 1.25z' /></svg></button>
+        <div class="word-card" v-if="!revers_card">
+            <div class="word-answer">
+                <input autofocus @keydown.enter="check_ans()" type="text" name="word" id="word" v-model="word_ans">
+            <div class="word-type">({{ words_arr[index].type }})</div>
+            </div>
             <div class="word-definition"> {{ words_arr[index].definition }}</div>
             <div class="word-examples"> {{ words_arr[index].examples }}</div>
-            
-    
             <button @click="check_ans(words_arr[index].word)">Check</button>
         </div>
-        <div class="word-card-back" v-if="revers_card">
-            <div class="word-type">{{ words_arr[index].type }}</div>
-            <div class="word-answer">{{ words_arr[index].word }}</div>
+        <div class="word-card" v-if="revers_card">
+            <div class="word-answer">
+                <input type="text" name="word" id="word_ans" disabled :value="words_arr[index].word">
+            <div class="word-type">({{ words_arr[index].type }})</div>
+            </div>
             <div class="word-definition"> {{ words_arr[index].definition }}</div>
             <div class="word-examples"> {{ words_arr[index].examples }}</div>
+            <button @click="check_ans(words_arr[index].word)">Skip</button>
         </div>
     
     </div>
 </template>
+
+<style scoped>
+
+.card {
+    width: 90vw;
+    height: 90vh;
+    margin: auto;
+    background-color: rgb(70, 218, 94);
+    display: flex;
+    align-content: center;
+    position: absolute;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    position: absolute;
+    border-radius: 20px;
+}
+
+.word-card {
+    width: 90%;
+    height: 90%;
+    display: flex;
+    align-content: space-between;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: center;
+}
+
+.reverse-btn {
+    width: 2.5rem;
+    height: 2.5rem;
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    padding: 0;
+    background-color: transparent;
+    border-width: 0;
+    border-radius: 50%;
+}
+
+.reverse-btn:hover {
+    background-color:  rgb(124, 231, 142);
+}
+
+.word-answer {
+    width: 100%;
+    margin: auto;
+    margin-top: 25px;
+}
+
+input {
+    width: 100%;
+    text-align: center;
+    border-width: 0;
+    background-color: rgb(178, 244, 180);
+    border-radius: 25px;
+    height: 3rem;
+    font-size: 2rem;
+}
+
+input:focus {
+    outline: none;
+    background-color: rgb(228, 245, 226);
+}
+
+button{
+    width: 100%;
+    margin: auto;
+    height: 3rem;
+    font-size: 2rem;
+    border-width: 0;
+    background-color: rgb(10, 193, 10);
+    border-radius: 25px;
+}
+
+.word-type {
+    width: fit-content;
+    margin: auto;
+    font-style: italic;
+    font-size: 1.5rem;
+}
+
+.word-definition, .word-examples {
+    width: 100%;
+    text-align: center;
+}
+
+
+</style>
