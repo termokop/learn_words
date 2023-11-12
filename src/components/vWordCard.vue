5e813277eb489fd7110155dbe43e11c3adc05bea
termokop <termokop564@gmail.com>
2023-11-12 04:55:59 -0700

    add cool animation for card word

--- src/components/vWordCard.vue
+++ src/components/vWordCard.vue
@@ -116,71 +116,129 @@
 </script>
 
 <template>
-    <div class="card">
+    <Transition mode="out">
+    <div class="card"  v-if="!revers_card">
         <button class="close-btn" @click="this.$router.push('/')">x</button>
         <button class="reverse-btn" @click="revers_card=!revers_card"><svg fill="white" width="20px" height="20px" viewBox="-1 -4.5 24 24" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin" class="jam jam-refresh-reverse"><path d='M4.859 5.308l1.594-.488a1 1 0 0 1 .585 1.913l-3.825 1.17a1 1 0 0 1-1.249-.665L.794 3.413a1 1 0 1 1 1.913-.585l.44 1.441C5.555.56 10.332-1.035 14.573.703a9.381 9.381 0 0 1 5.38 5.831 1 1 0 1 1-1.905.608A7.381 7.381 0 0 0 4.86 5.308zm12.327 8.195l-1.775.443a1 1 0 1 1-.484-1.94l3.643-.909a.997.997 0 0 1 .61-.08 1 1 0 0 1 .84.75l.968 3.88a1 1 0 0 1-1.94.484l-.33-1.322a9.381 9.381 0 0 1-16.384-1.796l-.26-.634a1 1 0 1 1 1.851-.758l.26.633a7.381 7.381 0 0 0 13.001 1.25z' /></svg></button>
-        <div class="word-card" v-if="!revers_card">
-            <div class="word-answer">
-                <input 
-                    autofocus 
-                    @keydown.enter="check_ans(words_arr[index].word)" 
-                    type="text" 
-                    name="word" 
-                    id="word" 
-                    v-model="word_ans"
-                    autocomplete="off"
-                />
-            <div class="word-type">({{ words_arr[index].type }})</div>
-            </div>
-            <div class="scroll-area">
-                <div class="word-definition"> 
-                    Definitions:
-                    <ul>
-                        <li v-for="definition in words_arr[index].definitions" :key="definition"> {{ definition }}</li>
-                    </ul>
-                </div>
-                <div class="word-examples">
-                    Examples:
-                    <ul>
-                        <li v-for="example in words_arr[index].examples" :key="example"> {{ example }}</li>
-                    </ul>
-                </div>
-            </div>
-            <button 
-                class="check-ans" 
-                @click="check_ans(words_arr[index].word)"
-                :disabled="!word_ans"
-            >
-                Check
-            </button>
-        </div>
-        <!-- Reverse card  -->
-        <div class="word-card" v-if="revers_card">
-            <div class="word-answer">
-                <input type="text" name="word" id="word_ans" disabled :value="words_arr[index].word">
+
+            <div class="word-card">
+                <div class="word-answer">
+                    <input 
+                        autofocus 
+                        @keydown.enter="check_ans(words_arr[index].word)" 
+                        type="text" 
+                        name="word" 
+                        id="word" 
+                        v-model="word_ans"
+                        autocomplete="off"
+                    />
                 <div class="word-type">({{ words_arr[index].type }})</div>
-            </div>
-            <div class="scroll-area">
-                <div class="word-definition"> 
-                    Definitions:
-                    <ul>
-                        <li v-for="definition in words_arr[index].definitions" :key="definition"> {{ definition }}</li>
-                    </ul>
                 </div>
-                <div class="word-examples">
-                    Examples:
-                    <ul>
-                        <li v-for="example in words_arr[index].examples" :key="example"> {{ example }}</li>
-                    </ul>
+                <div class="scroll-area">
+                    <div class="word-definition"> 
+                        Definitions:
+                        <ul>
+                            <li v-for="definition in words_arr[index].definitions" :key="definition"> {{ definition }}</li>
+                        </ul>
+                    </div>
+                    <div class="word-examples">
+                        Examples:
+                        <ul>
+                            <li v-for="example in words_arr[index].examples" :key="example"> {{ example }}</li>
+                        </ul>
+                    </div>
                 </div>
+                <button 
+                    class="check-ans" 
+                    @click="check_ans(words_arr[index].word)"
+                    :disabled="!word_ans"
+                >
+                    Check
+                </button>
             </div>
-            <button class="check-ans" @click="skip_word()">Skip</button>
-        </div>
-    
     </div>
+    </Transition>
+
+    <Transition mode="out-in">
+    <div class="card" v-if="revers_card">
+        <button class="close-btn" @click="this.$router.push('/')">x</button>
+        <button class="reverse-btn" @click="revers_card=!revers_card"><svg fill="white" width="20px" height="20px" viewBox="-1 -4.5 24 24" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin" class="jam jam-refresh-reverse"><path d='M4.859 5.308l1.594-.488a1 1 0 0 1 .585 1.913l-3.825 1.17a1 1 0 0 1-1.249-.665L.794 3.413a1 1 0 1 1 1.913-.585l.44 1.441C5.555.56 10.332-1.035 14.573.703a9.381 9.381 0 0 1 5.38 5.831 1 1 0 1 1-1.905.608A7.381 7.381 0 0 0 4.86 5.308zm12.327 8.195l-1.775.443a1 1 0 1 1-.484-1.94l3.643-.909a.997.997 0 0 1 .61-.08 1 1 0 0 1 .84.75l.968 3.88a1 1 0 0 1-1.94.484l-.33-1.322a9.381 9.381 0 0 1-16.384-1.796l-.26-.634a1 1 0 1 1 1.851-.758l.26.633a7.381 7.381 0 0 0 13.001 1.25z' /></svg></button>
+        <!-- Reverse card  -->
+        
+            <div class="word-card" >
+                <div class="word-answer">
+                    <input type="text" name="word" id="word_ans" disabled :value="words_arr[index].word">
+                    <div class="word-type">({{ words_arr[index].type }})</div>
+                </div>
+                <div class="scroll-area">
+                    <div class="word-definition"> 
+                        Definitions:
+                        <ul>
+                            <li v-for="definition in words_arr[index].definitions" :key="definition"> {{ definition }}</li>
+                        </ul>
+                    </div>
+                    <div class="word-examples">
+                        Examples:
+                        <ul>
+                            <li v-for="example in words_arr[index].examples" :key="example"> {{ example }}</li>
+                        </ul>
+                    </div>
+                </div>
+                <button class="check-ans" @click="skip_word()">Skip</button>
+            </div>    
+    </div>
+</Transition>
 </template>
 
 <style scoped>
+
+@keyframes loading1 {
+  0% {
+    opacity: 0;
+    transform: perspective(1000px) rotateX(0deg) rotateY(0deg);
+  }
+
+
+  50% {
+    opacity: 0;
+    scale: .5;
+    transform: perspective(1000px) rotateX(0deg) rotateY(-90deg);
+  }
+
+  100% {
+    opacity: 1;
+    transform: perspective(1000px) rotateX(0deg) rotateY(0deg);
+  }
+}
+
+@keyframes loading2 {
+  0% {
+    opacity: 1;
+    transform: perspective(1000px) rotateX(0deg) rotateY(-0deg);
+  }
+
+  50% {
+    opacity: 0;
+    scale: .5;
+    transform: perspective(1000px) rotateX(0deg) rotateY(-90deg);
+  }
+
+
+  100% {
+    opacity: 0;
+    transform: perspective(1000px) rotateX(0deg) rotateY(-0deg);
+  }
+}
+
+.v-enter-active {
+    animation: loading1 1s ease;
+}
+
+
+.v-leave-active {
+    animation: loading2 1s ease;
+}
+
 
 .card {
     width: 90vw;
@@ -199,6 +257,7 @@
 }
 
 .word-card {
+    position: absolute;
     width: 90%;
     height: 90%;
     display: flex;
